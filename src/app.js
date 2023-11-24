17072d6e5fdee715728c2a7d5ddf8cc56c2cecf0
Cory Rea <creaz103@gmail.com>
2023-11-23 20:17:37 -0600

    Finished (or almost finished) front end stuff

--- src/app.js
+++ src/app.js
@@ -1,67 +1,153 @@
-import TextField from "@mui/material/TextField";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
+import {
+    TextField,
+    Button,
+    Slider,
+    Input,
+    Typography,
+    Switch,
+    FormControlLabel,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { useEffect } from "react";
-
+import image from "./Resources/Letter C Profile Picture.jpg";
 import "./style.scss";
-import { Button } from "@mui/material";
-import backgroundImage from "./Resources/Letter C Profile Picture.jpg";
 
-// this is a theme that we can use to make our app dark
 const darkTheme = createTheme({
-	palette: {
-		mode: "dark",
-	},
+    palette: {
+        mode: "dark",
+    },
 });
 
-// this is the main component of our app
+const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    padding: "20px",
+    backgroundColor: darkTheme.palette.background.default,
+};
+
+const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+};
+
+const buttonStyle = {
+    marginTop: "20px",
+};
+
 function App() {
-    // this is a state variable that we can change
-	const [test, setTest] = React.useState("test");
+    const [prompt, setPrompt] = useState("");
+    const [numSteps, setNumSteps] = useState(1);
+    const [negativePrompt, setNegativePrompt] = useState("");
+    const [numFrames, setNumFrames] = useState(1);
+    const [showImage, setShowImage] = useState(false);
+    const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
+
+    const handleSubmit = () => {
+        setShowImage(true);
+    };
+
+    const handleRestart = () => {
+        setShowImage(false);
+        setShowAdvancedOptions(false); // Reset the advanced options state
+    };
 
     useEffect(() => {
-        console.log("This happens when this component renders!")
-    }, [])
+        console.log("This happens when this component renders!");
+    }, []);
 
-    useEffect(() => {
-        console.log("This happens when test changes!")
-    }, [test])
-
-    // this is the HTML that will be rendered
-    // all JS code must be inside curly braces!
     return (
-        <div>
-            <img
-                src={backgroundImage}
-                alt="Background"
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                }}
-            />
-            <h1 style={{ color: "#000" }}>Goodbye World</h1>
-
-            <TextField
-                label="Test"
-                value={test}
-                onChange={(e) => setTest(e.target.value)}
-            />
-
-            <br />
-
-            <p style={{ color: "#000" }}>{test}</p>
-
-            <br />
-
-            <Button variant="contained" color="primary">
-                Click Me!
-            </Button>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <div style={containerStyle}>
+                {showImage ? (
+                    <div>
+                        <img src={image} alt="Profile" />
+                    </div>
+                ) : (
+                    <div style={formStyle}>
+                        <CssBaseline />
+                        <Typography variant="h4" gutterBottom>
+                            SceneFusion
+                        </Typography>
+                        <TextField
+                            label="Prompt"
+                            value={prompt}
+                            onChange={(e) => setPrompt(e.target.value)}
+                        />
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={showAdvancedOptions}
+                                    onChange={() =>
+                                        setShowAdvancedOptions(!showAdvancedOptions)
+                                    }
+                                    color="primary"
+                                />
+                            }
+                            label="Advanced Options"
+                        />
+                        {showAdvancedOptions && (
+                            <>
+                                <TextField
+                                    label="Negative Prompt"
+                                    value={negativePrompt}
+                                    onChange={(e) => setNegativePrompt(e.target.value)}
+                                />
+                                <Typography variant="h6" gutterBottom>
+                                    Number of Steps:
+                                </Typography>
+                                <Slider
+                                    value={numSteps}
+                                    onChange={(e, value) => setNumSteps(value)}
+                                    min={1}
+                                    max={10}
+                                    step={1}
+                                    marks
+                                    valueLabelDisplay="auto"
+                                />
+                                <Typography variant="h6" gutterBottom>
+                                    Number of Frames:
+                                </Typography>
+                                <Slider
+                                    value={numFrames}
+                                    onChange={(e, value) => setNumFrames(value)}
+                                    min={1}
+                                    max={10}
+                                    step={1}
+                                    marks
+                                    valueLabelDisplay="auto"
+                                />
+                            </>
+                        )}
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            style={buttonStyle}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </div>
+                )}
+                {showImage && (
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={buttonStyle}
+                        onClick={handleRestart}
+                    >
+                        Restart
+                    </Button>
+                )}
+            </div>
+        </ThemeProvider>
     );
 }
 
-// this is the code that renders the HTML
 render(<App />, document.getElementById("root"));
