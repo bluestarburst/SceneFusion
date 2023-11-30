db608f7f3dc8dd425ea2e2e1378f55291b416b65
Bryant Hargreaves <hargreaves.bryant@gmail.com>
2023-11-30 12:44:05 -0600

    fixes

--- webpack.docs.js
+++ webpack.docs.js
@@ -42,6 +42,13 @@ function createCopy() {
         ],
         experiments: {
             topLevelAwait: true
+        },
+        resolve: {
+            fallback: {
+                "https": require.resolve("https-browserify"),
+                "http": require.resolve("stream-http"),
+                "url": require.resolve("url/"),
+            } 
         }
     })
 }
